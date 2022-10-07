a1a0bb32f69e7a74f4cc87d8f05c0e5db878873e
SIDDK24 <skoyande17@gmail.com>
2022-10-07 17:30:14 +0530

    Encryption Fix & Minor Update

--- device.mk
+++ device.mk
@@ -45,7 +45,12 @@ PRODUCT_PACKAGES += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-service \
+    android.hardware.boot@1.1-impl-wrapper.recovery \
+    android.hardware.boot@1.1-impl-wrapper \
+    android.hardware.boot@1.1-impl.recovery \
+    bootctrl.lahaina \
     bootctrl.lahaina.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
@@ -58,6 +63,10 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
+
+# Keystore
+PRODUCT_PACKAGES += \
+    android.system.keystore2
 
 # Screen
 TARGET_SCREEN_HEIGHT := 2400
@@ -90,14 +99,17 @@ TW_INCLUDE_RESETPROP := true
 TW_INCLUDE_REPACKTOOLS := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
-TW_Y_OFFSET := 80
-TW_H_OFFSET := -80
+#TW_Y_OFFSET := 80
+#TW_H_OFFSET := -80
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 TARGET_USES_MKE2FS := true
 TW_NO_SCREEN_BLANK := true
 TW_EXCLUDE_APEX := true
-TW_FRAMERATE := 60
+TW_FRAMERATE := 90
+#TW_LOAD_VENDOR_MODULES := "machine_dlkm.ko mxl5007t.ko rmnet_shs.ko tua9001.ko adsp_loader_dlkm.ko max2165.ko native_dlkm.ko rx_macro_dlkm.ko tuner-simple.ko apr_dlkm.ko mbhc_dlkm.ko pinctrl_lpi_dlkm.ko si2157.ko tuner-types.ko bolero_cdc_dlkm.ko mc44s803.ko pinctrl_wcd_dlkm.ko slimbus-ngd.ko tuner-xc2028.ko bt_fm_slim.ko platform_dlkm.ko slimbus.ko tx_macro_dlkm.ko btpower.ko q6_dlkm.ko snd_event_dlkm.ko va_macro_dlkm.ko e4000.ko q6_notifier_dlkm.ko stub_dlkm.ko wcd937x_dlkm.ko fc0011.ko q6_pdr_dlkm.ko swr_ctrl_dlkm.ko wcd937x_slave_dlkm.ko fc0012.ko qcom_edac.ko swr_dlkm.ko wcd938x_dlkm.ko fc0013.ko msi001.ko qm1d1b0004.ko swr_dmic_dlkm.ko wcd938x_slave_dlkm.ko fc2580.ko msm_drm.ko qm1d1c0042.ko swr_haptics_dlkm.ko wcd9xxx_dlkm.ko fts_tp.ko mt2060.ko qt1010.ko tda18212.ko wcd_core_dlkm.ko goodix_fp.ko mt2063.ko r820t.ko tda18218.ko wsa883x_dlkm.ko hdmi_dlkm.ko mt20xx.ko radio-i2c-rtc6226-qca.ko tda18250.ko wsa_macro_dlkm.ko hid-aksys.ko mt2131.ko rdbg.ko tda9887.ko xc4000.ko it913x.ko mt2266.ko rmnet_core.ko tea5761.ko xc5000.ko llcc_perfmon.ko mxl301rf.ko rmnet_ctl.ko tea5767.ko m88rs6000t.ko mxl5005s.ko rmnet_offload.ko tfa98xx_dlkm.ko"
+TW_OVERRIDE_SYSTEM_PROPS := \
+    "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 
 # Crypto
 TW_INCLUDE_CRYPTO := true
@@ -117,8 +129,7 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     libdisplayconfig.qti \
     libion \
     vendor.display.config@1.0 \
-    vendor.display.config@2.0 \
-    libdisplayconfig.qti
+    vendor.display.config@2.0
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
@@ -129,3 +140,6 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
 PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
+
+# VINTF
+PRODUCT_ENFORCE_VINTF_MANIFEST := true
